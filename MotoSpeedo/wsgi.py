11f86d431cc680f58a5320775eef0c4e62a1cec6
Kim Rypstra <kim.rypstra@gmail.com>
2018-07-06 21:06:00 +0800

    Added WSGI configuration and changed some settings for deployment

--- MotoSpeedo/wsgi.py
+++ MotoSpeedo/wsgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import time
+import traceback
+import signal
+import sys
+
 
 from django.core.wsgi import get_wsgi_application
+sys.path.append('/var/www/vhosts/motospeedo')
+sys.path.append('/var/www/vhosts/motospeedo/MotoSpeedoAPI/venv/lib/python3.4/site-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MotoSpeedo.settings")
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception:
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
